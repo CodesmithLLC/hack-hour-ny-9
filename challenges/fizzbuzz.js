b4416723d0240c76ac06380b3f3dfec749025467
Annette Lin <al261310@gmail.com>
2019-01-09 09:45:55 -0500

    hack-hr

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -20,9 +20,9 @@ function fizzbuzz(num) {
   let result = [];
 
   for(let i = 1; i <= num; i++){
-    if (i % 3 === 0) result.push("fizz");
+    if ((i % 5 === 0) && (i % 3 === 0)) result.push("fizzbuzz");
     else if (i % 5 === 0) result.push("buzz");
-    else if (i % 5 === 0 && i % 3 === 0) result.push("fizzbuzz");
+    else if (i % 3 === 0) result.push("fizz");
     else result.push(i)
   }
 
