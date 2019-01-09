28e9667e011203d2d2898cce085df907787959c8
Jinsung Park <js.lia.park@nyu.edu>
2019-01-09 09:29:27 -0500

    finished

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,23 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+let output = []
+    for(let i = 1; i <= num; i++){
+        if(i % 3 ===0){
+            if(i % 5 ===0){
+                output.push('fizzbuzz')
+            } else {
+                output.push('fizz')
+            }
+        } else if (i % 5 ===0){
+            output.push('buzz')
+        } else {
+            output.push(i)
+        }
+    }
+    return output;
 }
+
+console.log(fizzbuzz(16))
 
 module.exports = fizzbuzz;
