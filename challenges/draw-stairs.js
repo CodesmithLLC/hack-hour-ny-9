44d29ea19c4f6ba14d8a14344113a17ef38c6a33
Gaber Mowiena <gabermowiena@Gabers-MacBook-Air.local>
2019-01-11 10:21:30 -0500

    draw stairs

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,9 +13,13 @@
 
 */
 
-function drawStairs(n) {
-
+function drawStairs(n, steps = "*") {
+    if ( n >= 1 && n <= 100) {return steps}
+    let newN = n + 1;
+    steps += "*";
+    return drawStairs(newN , steps);
 }
+console.log(drawStairs(6));
 
 
 module.exports = drawStairs;
