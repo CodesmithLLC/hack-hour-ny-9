d971cf48b7c596182d48ae6764fca1a979cb0700
kiacolbert <colber16@gmail.com>
2019-01-11 11:45:52 -0500

    It works.

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,29 @@
 */
 
 function drawStairs(n) {
-
-}
-
+ //start at one
+ //place what i want to print in const - star - space
+ // iterate up to num for length
+  // interate up to num for width
+    //test if i +j ===num
+      //print starts
+    //else print space
+    //newline
+  const space = ' ';
+  const star = '*';
+  let line = '';
+  //let j = 1;
+  for(let i = 1; i <= n; i++) {
+    let numStars = i;
+    let numSpace = n - i;
+    for(let j = 1; j <= numSpace; j++){
+        line+= space;
+    }
+    for(let k = 1; k <= numStars; k++) {
+        line += star;
+    }
+    line='';
+  }
+};
 
 module.exports = drawStairs;
