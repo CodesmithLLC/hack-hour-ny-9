1e66331accef36ea1958a55e1307d5ce5cf3be8f
Andy Tran <andytran@Andy-Trans-MacBook-Pro.local>
2019-01-11 10:17:37 -0500

    completed challenge but will refactor later

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -12,10 +12,36 @@
 ******
 
 */
-
+// n = 6
+//start with n - space star and space (n - 1) spaces;
+//return the row
+//add 1 star and substract 1 space;
+//return the row
 function drawStairs(n) {
+    let space = ' ';
+    let star = '*';
+    let row = '';
+    let count = n - 1;
 
+    for (let x = 0; x < n; x++) {
+        for (let i = 0; i < count; i++) {
+            row += space;
+        }
+        for (let j = 0; j < n - count; j++) {
+            row += star;
+        } 
+        console.log(row);
+        row = '';
+        count -= 1;
+    }
 }
+
+// try to build a recursive/optimize previous solution
+// function drawStairs(n) {
+//     let space = ' ';
+//     let star = '*';
+// }
+// console.log(drawStairs(6));
 
 
 module.exports = drawStairs;
