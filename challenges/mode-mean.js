c0b5756584ec5d84620960079dfd6a24db96755a
Daniel Shu <shudaniel95@gmail.com>
2019-01-12 09:40:23 -0500

    Finished mode-mean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,31 @@
 
 
 function modemean(array) {
+  let mean = Math.floor(array.reduce((acc, curr) => acc + curr, 0)/array.length);
+  let obj = array.reduce(function(acc,curr) {
+    if (acc[curr]) {
+      acc[curr]++;
+      return acc;
+    } else {
+      acc[curr] = 1;
+      return acc;
+    }
+  }, {});
 
+  let entries = Object.entries(obj);
+  let maxCount = 0;
+  let mode;
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i][1] >= maxCount) {
+      maxCount = entries[i][1];
+      mode = entries[i][0];
+    }
+  }
+  // console.log(mode, mean);
+  return mode == mean;
+  // return Object.keys(obj)[values.indexOf(maxCount)] === mean;
 }
+// let arr = [1,1,1,1,1,1];
+// console.log(modemean(arr));
 
 module.exports = modemean;
