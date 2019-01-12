0fec21d6793ff7cd52b1a904196bcc149fd4c4b1
Serhii <35103391+SKaistrenko@users.noreply.github.com>
2019-01-12 09:52:56 -0500

    completed mode-mean and edited drawStairs to fit the test

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,19 @@
 
 
 function modemean(array) {
-
+  const count = {};
+  let sum = 0;
+  array.forEach(el => {
+    count[el] = count[el] ? count[el] + 1 : 1;
+    sum += el;
+  });
+  const mean = Math.floor(sum / array.length); 
+  const keys = Object.keys(count);
+  const mode = keys.reduce( (max, key) => {
+    if (count[key] > count[max] || (count[key] === count[max] && key > max)) return key;
+    return max;
+  })
+  return Number(mode) === mean;
 }
 
 module.exports = modemean;
