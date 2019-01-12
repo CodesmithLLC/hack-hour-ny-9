928eff05d2e87f49f6de9c29a5e70ffb2fd90256
kiacolbert <colber16@gmail.com>
2019-01-12 11:15:31 -0500

    In MeanMode

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,46 @@
 
 
 function modemean(array) {
+ // mean
+ let total = 0;
+ let mean = 0;
+ let modes = {};
+ mean = getMean();
 
+ //sort
+ array.sort((a, b) => a - b);
+
+ for (let i = 0; i < array.length; i++) {
+     if(modes.hasOwnProperty(array[i])) {
+        modes[array[i]]++;
+     } else {
+         modes[array[i]] = 1;
+     }    
+ }
+
+ for(let key in modes) {
+    if (modes[key] > 1) {
+      mode = 0 < modes[key] ? key : mode;
+      if(mode > 0){
+          mode = key;
+      } else {
+          if(mode < modes[key]){
+              mode = key;
+          }
+      }
+    }
+}
+
+if(mode) {
+    // keys are strings
+    return Number(mode)=== mean;
+}
+    function getMean() {
+        for (let i = 0; i < array.length; i += 1) {
+            total += array[i];
+        }
+        return Math.floor(total / array.length);
+    }
 }
 
 module.exports = modemean;
