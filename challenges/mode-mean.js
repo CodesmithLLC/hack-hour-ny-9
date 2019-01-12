2dae5344fa1f04b8956b8a39dddb04c0bc1085d6
wdanni <daniel.wu011@gmail.com>
2019-01-12 09:37:16 -0500

    completed, first submission

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,25 @@
 
 
 function modemean(array) {
-
-}
+  let len = array.length;
+  let mode = 0;
+  let mean = (array.reduce((acc, curr) => acc+=curr, 0)) / len;
+  console.log(mean);
+  
+  let freqobj = array.reduce((acc, curr) =>  {
+    acc[curr] ? acc[curr] += 1 :acc[curr] = 1;
+    return acc;
+  }, {})
+  console.log(freqobj)
+ 
+  for (let key in freqobj) {
+    if (freqobj[key] > mode) mode = key;  
+  }
+  
+  console.log(mode, mean)
+  
+  return mode == mean;
+    
+} 
 
 module.exports = modemean;
