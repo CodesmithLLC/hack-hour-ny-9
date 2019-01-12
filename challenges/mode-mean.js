05cec8ecc41972e20a46515af24e6035d423cf08
Victor Varaschin <vvaraschin@gmail.com>
2019-01-12 09:34:41 -0500

    finished

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,29 @@
 
 
 function modemean(array) {
+    const objOccurrences = array.reduce((accm, curr) => {
+        if (accm[curr] === undefined) accm[curr] = 1;
+        else accm[curr]++;
+        return accm;
+    }, {});
 
+    let mostFrequentOcc = 0;
+    for (let key in objOccurrences) {
+        mostFrequentOcc = Math.max(mostFrequentOcc, objOccurrences[key]);
+    }
+
+    const arrayOfModes = [];
+    for (let key in objOccurrences) {
+        if (objOccurrences[key] === mostFrequentOcc) arrayOfModes.push(parseFloat(key));
+    }
+
+    const maxMode = arrayOfModes.reduce((accm, curr) => Math.max(accm, curr));
+
+    const arraySum = array.reduce((accm, curr) => accm += curr);
+    const mean = Math.floor(arraySum / array.length); 
+
+    return mean === maxMode;
 }
+
 
 module.exports = modemean;
