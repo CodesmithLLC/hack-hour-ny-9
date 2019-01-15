7a759b01cf0096201cc40fe561dff8b5a1ec204b
Adele <adelecalvo@gmail.com>
2019-01-15 10:04:22 -0500

    for kth to the last attempt 2

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -13,18 +13,26 @@
 
 */
 
-function drawStairs(n, stairs='',count=1) {
- if (n===0) return stairs;
- for(let i=0; i<n-1; i++){
-    stairs+= ' ';
- }
- for (let j=0; j<count; j++) {
-    stairs+= '*'; 
- }
- if (count !==n) stairs+= '\n' // at 6 => 5 nbsp and 1 star but at 5 => 4 spaces and 2 stars 
+// function drawStairs(n, stairs='',count=1) {
+//  if (n===0) return stairs;
+//  for(let i=0; i<n-1; i++){
+//     stairs+= ' ';
+//  }
+//  for (let j=0; j<count; j++) {
+//     stairs+= '*'; 
+//  }
+//  if (count !==n) stairs+= '\n' // at 6 => 5 nbsp and 1 star but at 5 => 4 spaces and 2 stars 
 
-return drawStairs(n-1,stairs, count+1)
+// return drawStairs(n-1,stairs, count+1)
+// }
+console.log(drawStairs(6))
+
+function drawStairs(n) {
+   for (let i=1; i<n; i++){
+      const spaces = ' '.repeat(n-i);
+      const stars = '*'.repeat(i);
+   }
+   console.log( )
 }
-// console.log(drawStairs(6))
-
+console.log(drawStairs(6))
 module.exports = drawStairs;
--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -23,6 +23,41 @@ function Node(val) {
 
 function kthToLastNode(k, head) {
 
+  let currentNode = head; 
+  let cache = {}; 
+  let length = 0; 
+
+  // we know the head so we can search unil we reach this.next = null which is the tail and then 
+while(currentNode.next){
+  cache[length] = currentNode.value
+  length+=1; 
+  currentNode= currentNode.next
 }
+cache[length]= currentNode.value
+console.log(cache)
+return cache[length+1-k]
+
+  // somehow go backwards from that k times 
+    // well. we know how long it will take to get to the tail so we can do a cache for all the values and then retrive it 
+    // so 1st node is key 1 and its value and second node is key 2 value - etc and then we  know the length of the last node because its value is null and then subject n from that 
+
+}
+
+ const a = new Node('A');
+ const b = new Node('B');
+ const c = new Node('C');
+ const d = new Node('D');
+ const e = new Node ('E')
+
+
+ a.next = b;
+ b.next = c;
+ c.next = d;
+d.next =  e; 
+
+ console.log (kthToLastNode(4, a))
+ 
+
+
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
