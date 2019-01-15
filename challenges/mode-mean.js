f8b09c46b259568896f45e22e7848e52f28f9df5
Jinsung Park <js.lia.park@nyu.edu>
2019-01-15 09:33:24 -0500

    finished

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,38 @@
 
 
 function modemean(array) {
+let theMean = array[0]
+    for(let i = 1; i < array.length; i++){
+        theMean += array[i]
+    }
+    theMean = theMean/array.length
+    
+let counting = {}
+    for(let i = 0; i<array.length; i++){
+        if(!counting[array[i]]){
+            counting[array[i]] = 1
+            console.log(counting)
+        } else {
+            counting[array[i]] ++
+            console.log(counting)
+        }
+        
+    }
+let arr = Object.keys(counting)
+let theMode = arr[0]
 
+    for(let j = 0; j < arr.length; j++){
+        if(theMode < counting[arr[j]]){
+            theMode = counting[arr[j]]
+        } 
+    }
+
+    if((theMean) === theMode){
+        return true
+    } else {
+        return false
+    }
 }
 
 module.exports = modemean;
+
