dbe2ab8c72c3193e2f78462d1e5918201cea43d9
Julian King <julianking@Julians-MacBook-Pro-3.local>
2019-01-15 10:17:35 -0500

    kth to last complete

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -21,8 +21,43 @@ function Node(val) {
   this.next = null;
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+  
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+
 function kthToLastNode(k, head) {
+  let length = lengthCounter();
+  let node = head;
+  console.log(length)
+  
+  if (k > length) {return undefined}
+  for (let i = 1; i <= length - k; i++) {
+    node = node.next
+    console.log(node)
+
+  }
+  return node.value
+
+
+  function lengthCounter() {
+    let counter = 0;
+    let current = head;
+    while(current.next) {
+      counter++;
+      current = current.next;
+    }
+    counter++;
+    return counter;
+  }
 
 }
+console.log(kthToLastNode(6, a))
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
