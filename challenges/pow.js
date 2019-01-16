d699e3d1918d3b5c5d99c6d2844d355ded002d23
Annette Lin <al261310@gmail.com>
2019-01-16 09:45:28 -0500

    pow-hack-hr

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,6 +3,11 @@
  */
 
 function pow(base, power) {
+    if (power === 0) return 1;
+
+    if (power > 0) return base * pow(base, power -1);
+
+    else return 1 /(base * pow(base, Math.abs(power) -1));
 
 }
 
