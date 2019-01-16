7617432eb122520d5471afe03d65736697b406a3
kiacolbert <colber16@gmail.com>
2019-01-16 11:40:25 -0500

    Powers

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,14 @@
  */
 
 function pow(base, power) {
-
+// 3 * 3
+  if(power < 0) {
+    return;
+    }
+    if(power === 0 ) {
+        return 1;
+    }
+  return base * pow(base, power - 1);   
 }
-
+console.log(pow(3, 3));
 module.exports = pow;
