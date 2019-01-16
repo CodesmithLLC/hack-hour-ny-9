fc6d606b20b66966791d742f87c5208599faba2d
Andy Tran <andytran@Andy-Trans-MacBook-Pro.local>
2019-01-16 09:41:34 -0500

    completed pow

--- challenges/pow.js
+++ challenges/pow.js
@@ -2,8 +2,9 @@
  * Use recursion!
  */
 
-function pow(base, power) {
-
+function pow(base, power, result = 1) {
+    if (power < 1) return result;
+    return pow(base, power - 1, result *= base);
 }
 
 module.exports = pow;
