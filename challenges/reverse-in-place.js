c9178022f9347376713da572e4eaae7aeba9ea0e
Victor Varaschin <vvaraschin@gmail.com>
2019-01-18 09:47:00 -0500

    done

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -14,7 +14,13 @@
  */
 
 function reverseInPlace(array) {
-
+    for (let i = 0; i < Math.floor((array.length - 1) / 2); i++) {
+        const endValue = array[array.length - 1 - i];
+        array[array.length - 1 - i] = array[i];
+        array[i] = endValue;
+    }
+    
+    return array;
 }
 
 module.exports = reverseInPlace;
