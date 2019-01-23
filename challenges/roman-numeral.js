01d95edc34d44cd6f793904992475c8f99646b82
Annette Lin <al261310@gmail.com>
2019-01-23 10:42:34 -0500

    roman-numerals

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,6 +18,34 @@
  */
 
 function romanNumeral(n) {
+    let numerals = {
+        M:1000,
+        CM:900,
+        D:500,
+        CD:400,
+        C:100,
+        XC:90,
+        L:50,
+        XL:40,
+        X:10,
+        IX:9,
+        V:5,
+        IV:4,
+        I:1
+      }
+    
+    let result = ''
+    
+    for (let key in numerals) {
+    
+        if (numerals[key] === n) return key;
+    
+        while (n >= numerals[key]) {
+          result += key
+          n -= numerals[key]
+        }
+      }
+    return result
 
 }
 
