552a2ff5fca55edd61cd4e8b5a91ecd897f81941
Carson Chen <carson.cy.chen@gmail.com>
2019-01-23 10:06:51 -0500

    completed romanNumeral

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,43 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
 
+  let inputNum = n;
+
+  const conversions = [
+    { '1000': 'M' },
+    { '900': 'CM' },
+    { '500': 'D' },
+    { '400': 'CD' },
+    { '100': 'C' },
+    { '90': 'XC' },
+    { '50': 'L' },
+    { '40': 'XL' },
+    { '10': 'X' },
+    { '9': 'IX' },
+    { '5': 'V' },
+    { '4': 'IV' },
+    { '1': 'I' }
+  ]
+
+  let resultStr = '';
+
+  for (let i = 0; i < conversions.length; i++) {
+    if (inputNum === 0) return;
+    const curConKey = Object.keys(conversions[i])[0];
+    const curConKeyInt = parseInt(curConKey);
+    const curConValue = conversions[i][curConKey];
+
+    while (inputNum >= curConKeyInt) {
+      resultStr = resultStr.concat(curConValue);
+      inputNum -= curConKeyInt;
+    }
+  }
+  return resultStr;
 }
 
 module.exports = romanNumeral;
