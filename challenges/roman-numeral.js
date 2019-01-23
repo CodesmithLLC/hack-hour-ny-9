d3c5784568cb644e5d8a8fb1af6fafaf7810d01f
Daniel Shu <shudaniel95@gmail.com>
2019-01-23 09:57:28 -0500

    Finished

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -2,8 +2,10 @@
  * Some basic conversions are given below:
  *
  *      1     ->    I
+ *      3     ->    III
  *      4     ->    IV
  *      5     ->    V
+ *      8     ->    X
  *      9     ->    IX
  *      10    ->    X
  *      40    ->    XL
@@ -18,7 +20,34 @@
  */
 
 function romanNumeral(n) {
-
+  let num = n;
+  let result = "";
+  let results = {
+    1:"I",
+    4:"IV",
+    5:"V",
+    9:"IX",
+    10:"X",
+    40:"XL",
+    90:"XC",
+    100:"C",
+    400:"CD",
+    500:"D",
+    900:"CM",
+    1000:"M"
+  }
+  let keys = Object.keys(results);
+  while (num > 0) {
+    for (let i = keys.length-1; i >= 0; i--) {
+      if (num-keys[i] >= 0) {
+        num-=keys[i];
+        result+=results[keys[i]];
+      }
+    }
+  }
+  return result;
 }
+
+// console.log(romanNumeral(440));
 
 module.exports = romanNumeral;
