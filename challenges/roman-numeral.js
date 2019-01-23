124470442f7b8330f133757349973939b8214273
Serhii <35103391+SKaistrenko@users.noreply.github.com>
2019-01-23 10:20:43 -0500

    refactored array structure

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,18 +18,22 @@
  */
 
 function romanNumeral(n) {
-  const symbolsArr = [[1, 'I'],[4, 'IV'],[5, 'V'],[9, 'IX'],[10, 'X'],[40, 'XL'],[50, 'L'],[90, 'XC'],[100, 'C'],[400, 'CD'],[500, 'D'],[900, 'CM'],[1000,'M']];
+  const symbolsArr = [
+    1, 'I', 4, 'IV', 5, 'V', 9, 'IX', 10, 'X', 40, 'XL', 50, 'L',
+    90, 'XC', 100, 'C', 400, 'CD', 500, 'D', 900, 'CM', 1000, 'M',
+  ];
   
-  let remainder = 0;
   let roman = '';
+  let remainder;
   let symbol;
 
-  for (let i = symbolsArr.length - 1; i >= 0; i--) {
-    symbol = symbolsArr[i];
-    if (n / symbol[0] >= 1) {
-      remainder = n % symbol[0];
+  for (let i = symbolsArr.length - 1; i >= 0; i-=2) {
+    value = symbolsArr[i-1];
+    if (n / value >= 1) {
+      symbol = symbolsArr[i]
+      remainder = n % value;
       n -= remainder;
-      roman += symbol[1].repeat(n / symbol[0]);
+      roman += symbol.repeat(n / value);
       n = remainder;
     }
   }
