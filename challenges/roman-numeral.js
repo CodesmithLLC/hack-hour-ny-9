6e85fe9cc1427666ce6e8d28528ecff7cf49c541
Franklin Pinnock <pinnockg@gmail.com>
2019-01-23 09:23:24 -0800

    Finished roman-numeral.js

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -14,11 +14,39 @@
  *      500   ->    D
  *      900   ->    CM
  *      1000  ->    M
- * 
+ *
  */
 
 function romanNumeral(n) {
+  let num = n;
+  let result = '';
 
+  const converter = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+  ];
+
+  converter.forEach(
+    ([value, numeral]) => {
+      while (num > 0 && num >= value) {
+        num -= value;
+        result += numeral;
+      }
+    },
+  );
+
+  return result;
 }
 
 module.exports = romanNumeral;
