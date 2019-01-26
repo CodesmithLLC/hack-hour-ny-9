c68117182f1a139a613dbe58f1786cf73244adb8
Andy Tran <andytran@Andy-Trans-MacBook-Pro.local>
2019-01-26 10:31:34 -0500

    complete

---
+++ challenges/bin-to-dec.js
@@ -0,0 +1,31 @@
+/* You are given a string that represents a binary number
+ *
+ * Write a function that converts the binary string to a decimal number
+ *
+ * Example:
+* 	binToDec('0')   -> 0
+ * 	binToDec('11')  -> 3
+ * 	binToDec('100') -> 4
+ * 	binToDec('101') -> 5
+ *  binToDec('0101') -> 5
+ *
+ * Extension:
+ * Write a function that converts a decimal number to binary (then maybe hexadecimal)
+ */
+
+function binToDec(binary) {
+    let sum = 0;
+    let digit = 1;
+    for (let i = binary.length - 1; i >= 0; i--) {
+        if (binary[i] === '1') {
+            sum += digit;
+        } else if (binary[i] !== '0' && binary[i] !== '1') {
+            return "Error: Not in binary format"
+        }
+        digit *= 2;
+    }
+    return sum;
+}
+
+
+module.exports = binToDec;
