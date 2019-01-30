9db71386f41bd80c86cd3611ee0ce2b8f86c6eae
Victor Varaschin <vvaraschin@gmail.com>
2019-01-30 10:00:07 -0500

    handling negative numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -4,53 +4,44 @@
 
 function highestProduct(array) {
   if (array.length < 3 || !Array.isArray(array)) return 0;
-  const arrayOfMax = [];
+
+  const arrayOfMax = [Math.max(...array)];
+  
+  array.splice(array.indexOf(arrayOfMax[0]), 1);
   let arrayCopy = array.slice();
+  
+  // we need to check if the product of the smallest 2 is 
+  // greater than that of the greatest 2
+  const twoMin = [];
+  const twoMax = [];
 
-  const negativeNumCount = array.filter(el => el < 0).length;
-  if (negativeNumCount > 1) {
-    // we need to check if the product of the smallest 2 is 
-    // greater than that of the greatest 2
-    const twoMin = [];
-    const twoMax = [];
+  while(twoMin.length < 2) {
+    const currMin = Math.min(...arrayCopy);
+    twoMin.push(currMin);
+    arrayCopy.splice(arrayCopy.indexOf(currMin), 1);
+  }
+  
+  arrayCopy = array.slice();
+  while(twoMax.length < 2) {
+    const currMax = Math.max(...arrayCopy);
+    twoMax.push(currMax);
+    arrayCopy.splice(arrayCopy.indexOf(currMax), 1);
+  }
 
-    while(twoMin.length < 2 && arrayCopy.length !== 0) {
-      const currMin = Math.min(...arrayCopy);
-      twoMin.push(currMin);
-      arrayCopy.splice(arrayCopy.indexOf(currMin), 1);
-    }
-    
-    arrayCopy = array.slice();
-    while(twoMax.length < 2 && arrayCopy.length !== 0) {
-      const currMax = Math.max(...arrayCopy);
-      twoMax.push(currMax);
-      arrayCopy.splice(arrayCopy.indexOf(currMax), 1);
-    }
+  arrayCopy = array.slice();
 
-    arrayCopy = array.slice();
-
-    if (twoMin[0] * twoMin[1] > twoMax[0] * twoMax[1]) {
-      arrayOfMax.push(...twoMin);
-      arrayCopy.splice(array.indexOf(twoMin[0]), 1);
-      arrayCopy.splice(array.indexOf(twoMin[1]), 1);
-    } else {
-      arrayOfMax.push(...twoMax);
-      arrayCopy.splice(array.indexOf(twoMax[0]), 1);
-      arrayCopy.splice(array.indexOf(twoMax[1]), 1);
-    }
-
-    arrayOfMax.push(Math.max(...arrayCopy));
-
-    return arrayOfMax.reduce((a, b) => a * b);
+  if (twoMin[0] * twoMin[1] > twoMax[0] * twoMax[1]) {
+    arrayOfMax.push(...twoMin);
+    arrayCopy.splice(array.indexOf(twoMin[0]), 1);
+    arrayCopy.splice(array.indexOf(twoMin[1]), 1);
   } else {
-    while(arrayOfMax.length < 2 && arrayCopy.length !== 0) {
-      const currMax = Math.max(...arrayCopy);
-      arrayOfMax.push(currMax);
-      arrayCopy.splice(arrayCopy.indexOf(currMax), 1);
-    }
-
-    return arrayOfMax.reduce((a, b) => a * b);
-  }  
+    arrayOfMax.push(...twoMax);
+    arrayCopy.splice(array.indexOf(twoMax[0]), 1);
+    arrayCopy.splice(array.indexOf(twoMax[1]), 1);
+  }
+  
+  return arrayOfMax.reduce((a, b) => a * b);
 }
 
+console.log(highestProduct([1, 2, -10, 1,-2, 100]))
 module.exports = highestProduct;
