b9f3783e55521c34248162eccc5ed9e9c519ebd1
wdanni <daniel.wu011@gmail.com>
2019-01-30 09:32:58 -0500

    first submission

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,7 +3,10 @@
  */
 
 function highestProduct(array) {
-
+    if (array.length < 3) return undefined;
+    let sorted = array.sort((a,b) => b-a);
+    //console.log(sorted);
+    return sorted[0] * sorted[1] * sorted[2];    
 }
 
 
