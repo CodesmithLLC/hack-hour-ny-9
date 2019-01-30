5f7583ad308321ebca404ddd2dd970df64a6cdbb
Victor Varaschin <vvaraschin@gmail.com>
2019-01-30 09:30:40 -0500

    finished

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,14 @@
  */
 
 function highestProduct(array) {
+  const arrayOfMax = [];
+  while(arrayOfMax.length < 3 && array.length != 0) {
+    const currMax = Math.max(...array);
+    arrayOfMax.push(currMax);
+    array.splice(array.indexOf(currMax), 1);
+  }
 
+  return arrayOfMax.reduce((a, b) => a * b);
 }
-
 
 module.exports = highestProduct;
