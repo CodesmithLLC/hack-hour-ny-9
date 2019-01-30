a2b03314bb42510eac2cf74a560710051e82649d
Hubert <huberthflin@gmail.com>
2019-01-30 11:50:24 -0500

    .

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -8,24 +8,22 @@
 
 function highestProduct(array) {
   if (!Array.isArray(array) || array.length < 3) return 0;
-  array.sort((a, b) => a - b);
-
-  let maxProduct = 0;
-  let result = [];
-  for (let i = 0; i < array.length - 2; i += 1) {
-    let lo = i + 1;
-    let hi = array.length - 1;
-    const runningProduct = array[i] * array[lo] * array[hi];
-    if (runningProduct > maxProduct) {
-      result.push([array[i], array[lo], array[hi]]);
-      maxProduct = runningProduct;
-    } else {
-      lo += 1;
+  let maxProduct = -Infinity;
+  function dfs(nums, runningProduct, index, count) {
+    //base case
+    if (count === 3) {
+      maxProduct = Math.max(maxProduct, runningProduct);
+      return;
+    }
+    // else
+    for (let i = index; i < nums.length; i += 1) {
+      dfs(nums, runningProduct * nums[i], i + 1, count + 1);
     }
   }
+  dfs(array, 1, 0, 0);
   return maxProduct;
 }
 
-console.log(highestProduct([1, 2, 3]));
+console.log(highestProduct([1, 2, 3, 9]));
 
 module.exports = highestProduct;
