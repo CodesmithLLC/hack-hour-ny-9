bdcf0f938465fe72f221544ba64faa554d414cc6
Carson Chen <carson.cy.chen@gmail.com>
2019-02-02 10:17:14 -0500

    max stack completed

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,24 @@
 
 function Stack() {
   // body...
+  this.maxStack = [];
+  this.length = 0;
+  this.stack = [];
+  this.pop = function () {
+    this.length--;
+    this.maxStack.pop();
+    return this.stack.pop();
+  }
+  this.push = function (val) {
+    this.stack.push(val);
+    if (val > this.maxStack[this.maxStack.length - 1] || this.maxStack.length === 0) { this.maxStack.push(val) };
+    this.length++;
+    return this.length;
+  }
+  this.getMax = function () {
+    return this.maxStack[this.maxStack.length - 1];
+  }
 }
+
 
 module.exports = Stack;
