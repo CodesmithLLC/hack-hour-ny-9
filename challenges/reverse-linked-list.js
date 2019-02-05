c553895bac27d1ce01674c1f3b7295c796d8479b
Daniel Shu <shudaniel95@gmail.com>
2019-02-05 09:40:28 -0500

    Finished

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -32,17 +32,17 @@ function reverseLinkedList(head) {
     return currentTemp;
 }
 
-let newNode1 = new Node(5);
-let newNode2 = new Node(15);
-let newNode3 = new Node(25);
-let newNode4 = new Node(35);
-let newNode5 = new Node(45);
+// let newNode1 = new Node(5);
+// let newNode2 = new Node(15);
+// let newNode3 = new Node(25);
+// let newNode4 = new Node(35);
+// let newNode5 = new Node(45);
 
-newNode1.next = newNode2;
-newNode2.next = newNode3;
-newNode3.next = newNode4;
-newNode4.next = newNode5;
-console.log(reverseLinkedList(newNode1));
+// newNode1.next = newNode2;
+// newNode2.next = newNode3;
+// newNode3.next = newNode4;
+// newNode4.next = newNode5;
+// console.log(reverseLinkedList(newNode1));
 
 module.exports = { Node: Node, reverseLinkedList: reverseLinkedList };
 //
