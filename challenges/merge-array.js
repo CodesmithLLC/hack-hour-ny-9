3e7344b42ec084c6cf7b4d29c968425b0e4fde4e
Jinsung Park <js.lia.park@nyu.edu>
2019-02-08 10:39:09 -0500

    sad

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -17,19 +17,28 @@ function mergeArrays(arr1, arr2) {
     let output = []
 
     // basecase
-    if (arr1.length === 0 && arr2.length === 0) return output
-    if (arr1.length !== 0 && arr2.length === 0) {
+    if (arr1[0] === undefined && arr2[0] === undefined) {
+        console.log("check")
+        return output
+    } else if (arr1[0] !== undefined && arr2[0] === undefined) {
+        console.log("check")
         for (let i = 0; i < arr1.length; i++) {
             output.push(arr1[i])
             return output
         }
-    }
-    if (arr1.length === 0 && arr2.length !== 0) {
+
+    } else if (arr1[0] === undefined && arr2[0] !== undefined) {
+
+        console.log("check")
+        console.log(arr1)
+        console.log(arr2)
         for (let i = 0; i < arr2.length; i++) {
             output.push(arr2[i])
             return output
         }
     }
+
+    console.log(arr1[0] === undefined && arr2[0] !== undefined)
 
 
     if (arr1[0] < arr2[0]) {
@@ -39,7 +48,6 @@ function mergeArrays(arr1, arr2) {
     } else if (arr1[0] > arr2[0]) {
         output.push(arr2[0])
         arr2 = arr2.splice(1, arr1.length)
-
     }
 
     return output.concat(mergeArrays(arr1, arr2))
@@ -48,7 +56,7 @@ function mergeArrays(arr1, arr2) {
 
 module.exports = mergeArrays;
 
-
-var my_array = [3, 4, 6, 10, 11, 15, 21];
+var my_array = [3, 4, 6, 10, 11, 15];
 var another_array = [1, 5, 8, 12, 14, 19];
+
 console.log(mergeArrays(my_array, another_array))
--- challenges/num-to-words.js
+++ challenges/num-to-words.js
@@ -12,8 +12,111 @@
  * numToWords(92120000000000000) -> 'NintyTwoQuadrillionOneHundredTwentyTrillion'
  */
 
+
+function helper(arr) {
+    let digit = "1"
+    let array = []
+    array.push(Number(arr[arr.length - 1]))
+    for (let i = arr.length - 2; i > -1; i--) {
+        digit += "0"
+        console.log(digit)
+        console.log(arr[i])
+        array.push(arr[i] * digit)
+    }
+
+    return array.reverse();
+}
+
+
+
+
 function numToWords(num) {
+    let numbers = {
+        0: "Zero",
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        10: "Ten",
+        11: "Eleven",
+        12: "Twelve",
+        teen: "teen",
+        20: "Twenty",
+        30: "Thirty",
+        40: "Fourty",
+        50: "Fifty",
+        60: "Sixty",
+        70: "Seventy",
+        80: "Eighty",
+        90: "Ninety"
+    }
+    let overTwo = {
+        100: "Hundred",
+        1000: "Thousand",
+        1000000: "Million",
+        1000000000: "Billion",
+        1000000000000: "Trillion"
+    }
+    let output = ""
+
+
+    let str = num.toString()
+    let arr = str.split("")
+    console.log(arr)
+
+
+    for (let keys in numbers) {
+        if (keys === num.toString()) {
+            return numbers[keys]
+        }
+    }
+    let input = helper(arr)
+    console.log(input)
+
+    while (input.length !== 0) {
+        console.log(input.length)
+        for (let number of input) {
+            console.log(Object.keys(numbers))
+            console.log(number)
+            let string = number.toString()
+            // console.log(string === Object.keys(numbers))
+            console.log(string)
+            console.log(input)
+            for (let digits in overTwo) {
+                if (string.length === digits.length) {
+                    console.log(digits)
+                    console.log(string)
+                    output += (numbers[string[0]])
+
+
+                    output += overTwo[digits]
+
+
+                    console.log(input)
+                    console.log(output)
+                }
+            }
+            input = input.splice(1, 3)
+
+        }
+
+    }
 
 }
 
+
+
+
+
+
+
 module.exports = numToWords;
+
+console.log(numToWords(2999))
+// console.log(numToWords(29))
+console.log(numToWords(4))
