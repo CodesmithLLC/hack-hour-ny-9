cc95d3ca2714423ab8c54dad95e9e57679ada51e
Annette Lin <al261310@gmail.com>
2019-02-09 10:05:22 -0500

    permu

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -9,8 +9,19 @@
  *
  */
 
+function isPallindrome(str) {
+    if (str.length === 1) return false;
+    return str == str.split('').reverse().join('');
+  }
+  
 function permPalin(str) {
-	
+    if (str.length === 1) return true;
+  
+    for (let i = 0; i < str.length; i++){
+      let test = str.slice(i,i+3);
+      if (isPallindrome(test)) return true;
+    }
+return false;
 }
 
 module.exports = permPalin;
