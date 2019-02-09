368ed0b8b45107e16ea88ee7b7842a1d1a0c82a7
Andy Tran <andytran@Andy-Trans-MacBook-Pro.local>
2019-02-09 09:51:42 -0500

    completed challenge

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,26 @@
  */
 
 function permPalin(str) {
-	
+  const result = str.split('');
+  const obj = {};
+  for (let i = 0; i < result.length; i += 1) {
+    if (obj[result[i]]) {
+      obj[result[i]] += 1;
+    } else {
+      obj[result[i]] = 1;
+    }
+  }
+  const values = Object.values(obj);
+  let odd = false;
+  for (let i = 0; i < values.length; i += 1) {
+    if (values[i] % 2 !== 0 && odd === true) {
+      return false;
+    }
+    if (values[i] % 2 !== 0) {
+      odd = true;
+    }
+  }
+  return true;
 }
 
 module.exports = permPalin;
