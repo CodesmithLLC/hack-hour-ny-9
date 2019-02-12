9b9f0a1099c01f5e9a277c3c4edfdbc145a7c70a
Daniel Shu <shudaniel95@gmail.com>
2019-02-12 10:11:26 -0500

    Done

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,9 +4,22 @@
 
 
 function Stack() {
+  this.stck = [];
+
+  Stack.prototype.push = (val) => {
+    this.stck[this.stck.length] = val;
+  }
+
+  Stack.prototype.pop = () => {
+    if (this.stck.length > 0) {
+      this.returnValue = this.stck[this.stck.length - 1];
+      this.stck.length = this.stck.length - 1;
+      return returnValue;
+    }
+    return undefined;
+  }
 
 }
-
 
 /**
 * Queue Class
@@ -14,7 +27,23 @@ function Stack() {
 
 
 function Queue() {
+  this.stck1 = new Stack();
+  this.stck2 = new Stack();
 
+  Queue.prototype.enqueue = (val) => {
+    this.stck1.push(val);
+  }
+  Queue.prototype.dequeue = () => {
+    if (this.stck1.length === 0) return undefined;
+    if (this.stck2.length === 0) stck2.push(stck1.stck[0]);
+
+    if (this.stck2.length > 0) {
+      while (this.stck1.length > 0) {
+        this.stck2.push(this.stck1.pop());
+      }
+    }
+    return this.stck2.pop();
+  }
 }
 
-module.exports = {Stack: Stack, Queue: Queue};
+module.exports = { Stack: Stack, Queue: Queue };
