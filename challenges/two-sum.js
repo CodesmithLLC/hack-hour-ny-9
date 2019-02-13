34b51d99e6337b09e8dbdbaf48be1a032e7a262b
wdanni <daniel.wu011@gmail.com>
2019-02-13 09:38:39 -0500

    added false return

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,11 @@
  */
 
 function twoSum(arr, n) {
-
+  if (arr.length === 1) return true;
+  for (let i=0; i < arr.length; i++) {
+    if (Array.indexOf(n - arr[i]) >= 0) return true
+  }
+  return false;
 }
 
 module.exports = twoSum;
