0b37900a4a747bc8e0d48b68dcc7e970c24c0a70
Adele <adelecalvo@gmail.com>
2019-02-14 09:54:02 -0500

    valid-bst atttempt

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -4,16 +4,24 @@
  *      at any given node, the value of all the nodes in its right tree must be > its value
  * Assume that each value in the tree is unique.
  */
- 
 
 function BinaryTree(val) {
-    this.value = val;
-    this.left = null;
-    this.right = null;
+  this.value = val;
+  this.left = null;
+  this.right = null;
 }
 
 function validBST(tree) {
-
+  let currentNode = tree.value;
+  while (currentNode.left !== null) {
+    if (currentNode < currentNode.left.value) return false;
+    currentNode = currentNode.left;
+  }
+  while (currentNode.right !== null) {
+    if (currentNode > currentNode.right.value) return false;
+    currentNode = currentNode.right;
+  }
+  return true;
 }
 
-module.exports = {BinaryTree: BinaryTree, validBST: validBST};
+module.exports = { BinaryTree: BinaryTree, validBST: validBST };
