e1e6dec3ba013e41de97fd4df59bd3df6c203f13
Serhii <35103391+SKaistrenko@users.noreply.github.com>
2019-02-14 10:39:02 -0500

    tree att-2

--- challenges/valid-bst.js
+++ challenges/valid-bst.js
@@ -16,13 +16,24 @@ function validBST(tree) {
   function helper(tree) {
     if (!tree) return;
     const left = helper(tree.left)
-    if (left && left > tree.value) return false;
+    if (left && left >= tree.value) return false;
     const right = helper(tree.right)
-    if (right && right < tree.value) return false;
+    if (right && right <= tree.value) return false;
     return tree.value;
   }
   return helper(tree) !== false;
 }
+
+const tree = new BinaryTree(10);
+tree.left = new BinaryTree(8);
+tree.right = new BinaryTree(12);
+tree.left.left = new BinaryTree(6);
+tree.left.right = new BinaryTree(7);
+tree.left.left.left = new BinaryTree(1);
+tree.left.left.right = new BinaryTree(5);
+
+console.log(validBST(tree));
+
 
 module.exports = {
   BinaryTree: BinaryTree,
