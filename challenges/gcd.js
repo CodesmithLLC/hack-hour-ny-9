0e9ea6bdf2c27adfddb257b10ec967e0f8835efc
Adele <adelecalvo@gmail.com>
2019-02-15 09:48:35 -0500

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,15 @@
  */
 
 function gcd(a, b) {
-
+  let cap;
+  let result = 0;
+  if (a < 0 || b < 0) return false;
+  a > b ? (cap = a) : (cap = b);
+  for (let i = 1; i <= cap; i += 1) {
+    if (a % i === 0 && b % i === 0) {
+      result = i > result ? i : result;
+    }
+  }
+  return result > 0 ? result : null;
 }
-
 module.exports = gcd;
