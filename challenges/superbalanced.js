0f58e3bf7ba80a44762f4d4c1ae64d0588898869
Daniel Shu <shudaniel95@gmail.com>
2019-02-27 09:40:09 -0500

    Done

--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -1,5 +1,5 @@
 /**
- * Write a function to see if a binary tree "superbalanced".
+ * Write a function to see if a binary tree is "superbalanced".
  * An empty tree is balanced. A non-empty binary tree T is balanced if:
 1) Left subtree of T is balanced
 2) Right subtree of T is balanced
@@ -14,7 +14,20 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
-
+  function getHeight(node) {
+    if (!node) return 0;
+    return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+  }
+  if (Math.abs(getHeight(node.left) - getHeight(node.right) > 1)) return false;
+  return true;
 }
 
-module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
+// let bst = new BinaryTree(10);
+// bst.right = new BinaryTree(12);
+// bst.right.left = new BinaryTree(11);
+// bst.right.right = new BinaryTree(13);
+// bst.left = new BinaryTree(5);
+// bst.left.right = new BinaryTree(7);
+// bst.left.right.right = new BinaryTree(13);
+
+module.exports = { BinaryTree: BinaryTree, superbalanced: superbalanced };
