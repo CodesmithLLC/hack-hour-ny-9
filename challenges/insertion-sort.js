495d3998ecaf7292211343425d6e42d6af80002e
Victor Varaschin <vvaraschin@gmail.com>
2019-03-11 09:42:16 -0400

    done insertion

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,26 @@
 
 
 function insertionSort(array) {
+  function insert(array, value, index) {
+    array = [...array.slice(0, index), value, ...array.slice(index)];
+    return array;
+  }
 
+  if (array.length === 0) return [];
+  const arrLength = array.length;
+
+  for (let i = 1; i < arrLength; i++) {
+    const currentValue = array[i];
+    for (let j = 0; j < i; j++) {
+      if (currentValue < array[j]) {
+        array = insert(array, currentValue, j);
+        i++;
+      }
+    }
+  }
+
+  return array;
 }
 
+console.log(insertionSort([1, 5, 2, 3, 10]));
 module.exports = insertionSort;
