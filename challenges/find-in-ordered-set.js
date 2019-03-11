535bd130e2c9acd71f9a36612a797a02a2124c68
Jinsung Park <js.lia.park@nyu.edu>
2019-03-11 10:20:49 -0400

    insertionSort

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -11,8 +11,19 @@ findInOrderedSet(nums, 2);  -> false
 
 
 function findInOrderedSet(arr, target) {
+  let middle = Math.floor(arr.length / 2)
 
+  if (middle === 0) return false
+  if (target < arr[middle]) {
+    arr = arr.slice(0, middle)
+  } else if (target > arr[middle]) {
+    arr = arr.slice(middle, arr.length)
+  } else if (target === arr[middle]) {
+    return true;
+  }
+  return findInOrderedSet(arr, target)
 }
+console.log(findInOrderedSet([1, 4, 6, 7, 9, 17, 45], 2))
 
 
 module.exports = findInOrderedSet;
