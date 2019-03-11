d674e2da0f0ce54c69712fe7993a0cf7b0a1175f
Franklin Pinnock <pinnockg@gmail.com>
2019-03-10 17:51:06 -0700

    Finished two-sum.js

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -1,9 +1,16 @@
-/*  Given an array of numbers and a target number, return true if there are two numbers in the 
+/*  Given an array of numbers and a target number, return true if there are two numbers in the
  *  array that sum up to the target value; return false otherwise
  */
 
 function twoSum(arr, n) {
-
+  const seen = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (seen[arr[i]]) {
+      return true;
+    }
+    seen[n - arr[i]] = true;
+  }
+  return false;
 }
 
 module.exports = twoSum;
