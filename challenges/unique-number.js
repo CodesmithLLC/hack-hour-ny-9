03f49ff4f3aae6072a5057e47f5d1ee1758c748e
Daniel Shu <shudaniel95@gmail.com>
2019-03-12 11:08:01 -0400

    Done

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -3,6 +3,8 @@
  * except for one integer that will appear exactly once. Return the integer that appears once.
  *
  * uniqueNumber([1,2,1,3,3]); -> 2
+ * 1 + 1 + 3 + 3 + 2 = 10
+ * 
  *
  * BONUS:
  * Complete the challenge in O(n) time
@@ -10,7 +12,31 @@
  *
  */
 function uniqueNumber(array) {
-
+  // let sum = array.reduce((curr, acc) => {
+  //   return acc += curr;
+  // }, 0);
+  //o(n) just use an object to store the count times and at the end return the one with 1 count
+  let obj = {};
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i]]) obj[array[i]]++;
+    else obj[array[i]] = 1;
+  }
+  for (key in obj) {
+    // console.log(key, obj[key]);
+    if (obj[key] === 1) return key;
+  }
+  return;
+  //o(1) pointers?
+  // for (let i = 0; i < array.length; i++) {
+  //   let index = Math.abs(array[i]) - 1;
+  //   if (array[index] === Math.max()) array[index] = 0;
+  //   else array[index] = Math.max();
+  // }
+  // for (let j = 0; j < array.length; j++) {
+  //   if (array[j] === Math.max()) return 
+  // }
 }
+
+// console.log(uniqueNumber([1, 2, 1, 3, 3]));
 
 module.exports = uniqueNumber;
