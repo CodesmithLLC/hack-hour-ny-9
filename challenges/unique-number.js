312fecb8b8f3f9e94426b62bbc44c8e976c0973a
wdanni <daniel.wu011@gmail.com>
2019-03-12 09:27:43 -0400

    corrected arrow function in sort

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -13,7 +13,7 @@
  */
 function uniqueNumber(array) {
   // sorting yields n log(n) time complexity
-  let arr = array.sort((a,b) -> a-b);
+  let arr = array.sort((a,b) => a-b);
   for (let i=0; i < arr.length; i+=2) {
     if (arr[i] !== arr[i+1]) {
       return arr[i+1] === arr[i+2] ? arr[i] : arr[i+1];
