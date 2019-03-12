5c1876ed27e34842cb506e2ba56223345e589ab8
wdanni <daniel.wu011@gmail.com>
2019-03-12 09:25:26 -0400

    first submission

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -3,14 +3,22 @@
  * except for one integer that will appear exactly once. Return the integer that appears once.
  *
  * uniqueNumber([1,2,1,3,3]); -> 2
- *
+ * uniqueNumber([1,1,2,3,3]);
+   uniqueNumber([1,2,2,3,3]);
+   uniqueNumber([1,1,2,2,3])
  * BONUS:
  * Complete the challenge in O(n) time
  * Complete the challenge in O(1) space
  *
  */
 function uniqueNumber(array) {
-
+  // sorting yields n log(n) time complexity
+  let arr = array.sort((a,b) -> a-b);
+  for (let i=0; i < arr.length; i+=2) {
+    if (arr[i] !== arr[i+1]) {
+      return arr[i+1] === arr[i+2] ? arr[i] : arr[i+1];
+    }
+  }
 }
 
 module.exports = uniqueNumber;
