1d00566399a8d6e9b1f3df3a7ba51bbd40a23a85
wdanni <daniel.wu011@gmail.com>
2019-03-13 09:25:18 -0400

    first submission

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -10,7 +10,18 @@
  */
 
 function getAllProducts(array) {
-
+  if (array.filter(el => el === 0).length === 1) return [array.reduce((acc, curr) => curr === 0 ? acc : acc*curr, 1), 0];
+  if (array.filter(el => el === 0).length > 1) return [0];
+  let products = [];
+  for (let i=0; i < array.length; i++) {
+    let prodArray = array.slice(0,i).concat(array.slice(i+1));
+    products.push(prodArray.reduce((acc, curr) => acc*curr, 1));
+  }
+  const set = new Set(products);
+  return Array.from(set);
 }
-
+  
+getAllProducts([1, 7, 3, 4]);
+  
+  
 module.exports = getAllProducts;
