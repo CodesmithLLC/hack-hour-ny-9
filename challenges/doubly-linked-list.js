065dd8c8ef90aceb470aff582fc20d7e26bff8bb
Jinsung Park <js.lia.park@nyu.edu>
2019-03-15 09:16:41 -0400

    hi

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -16,15 +16,40 @@ function Node(val) {
 /*
 Adds a node to the end of the list
  */
-LinkedList.prototype.add = function(val) {
-  
+LinkedList.prototype.add = function (val) {
+  let current;
+  let before;
+  if (!this.head) {
+    this.head = new Node(val)
+    current = this.head
+    before = null
+  } else {
+    before = current
+    current.next = new Node(val)
+    current = current.next
+    current.prev = before
+    this.tail = current
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
-LinkedList.prototype.remove = function(val) {
-  
+LinkedList.prototype.remove = function (val) {
+  let current = this.head
+  let post = current.next
+  let pre = current.prev
+  if (this.head.val === val) {
+    post = this.head
+    return current.val
+  } else {
+    let post = current.next
+    let pre = current.prev
+    current = current.next
+    if (current.val === val) {
+
+    }
+  }
 };
 
 module.exports = LinkedList;
