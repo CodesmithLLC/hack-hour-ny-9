ac35277cf88b48301a8fdff28b89eb5cdeafe20e
Daniel Shu <shudaniel95@gmail.com>
2019-03-16 09:37:50 -0400

    Done

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,22 @@
 
 
 function bubbleSort(array) {
-
+  let temp;
+  let sorted = false;
+  while (!sorted) {
+    sorted = true;
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        temp = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = temp;
+        sorted = false;
+      }
+    }
+  }
+  return array;
 }
+
+// console.log(bubbleSort([5, 3, 2, 1]))
 
 module.exports = bubbleSort;
